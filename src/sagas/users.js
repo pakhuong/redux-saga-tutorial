4e4412154535117143be60e6cc3b43035e2fd74d
pakhuong <anhkhuong1602@gmail.com>
2018-12-26 20:22:12 +0700

    Finishing the get users saga

--- src/sagas/users.js
+++ src/sagas/users.js
@@ -1,15 +1,22 @@
-import { takeEvery } from 'redux-saga';
+import { call, fork, takeEvery } from 'redux-saga/effects';
 
 import * as actions from '../actions/users';
+import * as api from '../api/users';
 
 function* getUsers() {
     try {
 
     } catch (e) {
-        
+
     }
 }
 
 function* watchGetUsersRequest() {
     yield takeEvery(actions.Types.GET_USER_REQUEST, getUsers);
 }
+
+const usersSaga = [
+    fork(watchGetUsersRequest)
+];
+
+export default usersSaga;
