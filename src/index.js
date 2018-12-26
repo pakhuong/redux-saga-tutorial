01617819449f03db6e4665496a7a9775cc07d5e0
pakhuong <anhkhuong1602@gmail.com>
2018-12-26 20:28:38 +0700

    Connecting the saga middleware

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import axios from 'axios';
-import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { applyMiddleware, createStore } from 'redux';
+import createSagaMiddleware from 'redux-saga';
 
 import App from './components/App';
 import reducers from './reducers';
+import rootSaga from './sagas';
 import * as serviceWorker from './serviceWorker';
 
 axios.defaults.withCredentials = true;
 axios.defaults.baseUrl = 'http://rem-rest-api.herokuapp.com/api';
 
-const store = createStore(reducers);
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(reducers, applyMiddleware(sagaMiddleware));
+
+sagaMiddleware.run(rootSaga);
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
